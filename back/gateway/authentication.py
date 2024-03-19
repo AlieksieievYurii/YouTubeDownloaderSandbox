bfe3d3776ff2260ae1c75624a58859e5c33f1413
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2024-03-19 18:37:39 +0100

    Make the decorator requires_authentication to inject user details

--- back/gateway/authentication.py
+++ back/gateway/authentication.py
@@ -2,22 +2,26 @@
 
 import functools
 from http import HTTPStatus
+from typing import Tuple
 import requests
 import variables
 
 from flask import request
 
 
-def is_jwt_valid(jwt: str) -> bool:
+def validate_jwt(jwt: str) -> Tuple[bool, str]:
     """Validates the given JWT token. Using Auth service for that"""
-    respoce = requests.post(
+    response = requests.post(
         variables.AUTH_SERVICE_VALIDATE,
         headers={"Authorization": f"Bearer {jwt}"},
         timeout=None,
     )
-    if respoce.status_code != HTTPStatus.OK:
-        print(f"Token validation failed. Error<{respoce.status_code}>: {respoce.text}")
-    return respoce.status_code == HTTPStatus.OK
+    if response.status_code != HTTPStatus.OK:
+        print(
+            f"Token validation failed. Error<{response.status_code}>: {response.text}"
+        )
+
+    return response.status_code == HTTPStatus.OK, response.json()
 
 
 def requires_authentication(fun):
@@ -28,9 +32,9 @@ def requires_authentication(fun):
         jwt = request.cookies.get("JWT")
         if not jwt:
             return "Missing cookie with JWT", HTTPStatus.UNAUTHORIZED
-
-        if is_jwt_valid(jwt):
-            return fun(*args, **kwargs)
+        is_valid, data = validate_jwt(jwt)
+        if is_valid:
+            return fun(data, *args, **kwargs)
 
         return "JWT token is invalid", HTTPStatus.UNAUTHORIZED
 
