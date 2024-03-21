1e6ee43c80a1cd6a14ee3ae92770d2472ffd650b
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2024-03-21 13:54:09 +0100

    Move the mysql starup to compose

--- back/auth/run-db.sh
+++
@@ -1,2 +0,0 @@
-set -x
-docker run --name some-mysql --rm -e MYSQL_ROOT_PASSWORD=yurii -p 3306:3306 -v D:/Projects/YouTubeDownloader/back/auth/init.sql:/docker-entrypoint-initdb.d/init.sql mysql:latest
