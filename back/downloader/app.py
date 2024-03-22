3284aa99b2aa68586ba356d10ee1fc0bbb18180d
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2024-03-22 14:51:55 +0100

    Implement updating database in the downloader

--- back/downloader/app.py
+++ back/downloader/app.py
@@ -1,6 +1,15 @@
+""" Downloader is a service that listens for incoming messages from the
+RabbitQM. Once it receives 'job' containing YouTube video URL, it starts
+downloading audio file. Once the file is downloaded, the service updates
+an item in the database.
+"""
+
 import json
 from pathlib import Path
+import tempfile
+from gridfs import GridFS
 import pika
+from pymongo import MongoClient
 import youtube_downloader
 import variables
 
@@ -15,28 +24,81 @@ connection = pika.BlockingConnection(
 )
 channel = connection.channel()
 
+client = MongoClient(variables.MONGODB)
 
-def on_progress(downloaded: int, size: int):
-    print("Prog: ", end='')
-    print(size)
-    print(f"{size} / {downloaded}")
+fs_videos = GridFS(client.get_database("audio_files"))
+
+items_collection = client.get_database("main").get_collection("items")
+
+
+def set_item_status_as_downloading(video_id: str) -> None:
+    """Sets the item as downloading in the DB"""
+    items_collection.update_one(
+        {"video_id": video_id}, {"$set": {"state": "DOWNLOADING"}}
+    )
+
+
+def set_item_status_as_failed(video_id: str, message: str) -> None:
+    """Sets the item as failed"""
+    items_collection.update_one(
+        {"video_id": video_id},
+        {"$set": {"state": "FAILED", "error_message": message}},
+    )
+
+
+def set_item_status_as_downloaded(
+    audio_file_id: str, video_id: str, size: int
+) -> None:
+    """Sets the item as downloaded in the DB"""
+    items_collection.update_one(
+        {"video_id": video_id},
+        {
+            "$set": {
+                "audio_file_id": audio_file_id,
+                "state": "DOWNLOADED",
+                "size": size,
+            }
+        },
+    )
+
+
+def on_progress(video_id: str, downloaded: int, size: int):
+    pass
+    # TODO and test
+    # items_collection.update_one(
+    #     {"video_id": video_id}, {"$set": {"progress": downloaded}}
+    # )
+    # print("Prog: ", end="")
+    # print(size)
+    # print(f"{size} / {downloaded}")
 
 
 def on_reveice_job(ch, method, properties, body) -> None:
-    data = json.loads(body)
-    file = Path("output") / f"{data["video_id"]}.mp3"
-    print(f"start downloading {data["url"]}")
-    youtube_downloader.download(data["url"], file, on_progress)
+    """Callback function that is trigger once there is a job in queue"""
+    data: dict = json.loads(body)
+    video_id: str = data["video_id"]
+    origin_video_url = data["url"]
+    file = Path(tempfile.gettempdir()) / f"{video_id}.mp3"
+    print(f"Start downloading {origin_video_url}")
+    set_item_status_as_downloading(video_id)
+    try:
+        youtube_downloader.download(
+            origin_video_url, file, lambda d, s: on_progress(video_id, d, s)
+        )
+    except Exception as error:
+        print(error)
+        set_item_status_as_failed(video_id, str(set_item_status_as_failed))
+    uid = fs_videos.put(file.read_bytes())
+    set_item_status_as_downloaded(uid, video_id, file.stat().st_size)
 
 
 def main() -> None:
-    channel.basic_consume(
-        queue=variables.QUEUE_NAME, on_message_callback=on_reveice_job
-    )
+    """Entrypoint function"""
+    channel.basic_consume(queue="jobs", on_message_callback=on_reveice_job)
     print("Waiting for jobs...")
     channel.start_consuming()
 
 
 if __name__ == "__main__":
-    channel.queue_declare(queue=variables.QUEUE_NAME)
+    channel.queue_declare("jobs")
     main()
--- back/downloader/variables.py
+++ back/downloader/variables.py
@@ -19,4 +19,12 @@ RABBITMQ_HOST = _load_env_variable("RABBITQM_HOST", "localhost")
 RABBITMQ_SVC_USER = _load_env_variable("RABBITMQ_SVC_USER", "yurii")
 RABBITMQ_SVC_PASSWORD = _load_env_variable("RABBITMQ_SVC_PASSWORD", "yurii")
 
-QUEUE_NAME = "jobs"
+MONGODB_HOST = _load_env_variable("MONGODB_HOST", "localhost")
+MONGODB_PORT = _load_env_variable("MONGODB_PORT", "27017")
+MONGODB_USERNAME = _load_env_variable("MONGODB_USERNAME", "yurii")
+MONGODB_PASSWORD = _load_env_variable("MONGODB_PASSWORD", "yurii")
+
+MONGODB = (
+    f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}"
+    + f"@{MONGODB_HOST}:{MONGODB_PORT}"
+)
