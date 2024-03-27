dbb30b3d6755eaf9bd4806a48210bc460478be8b
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2024-03-27 14:17:37 +0100

    Add more tests

--- back/gateway/tests/test_authentication.py
+++ back/gateway/tests/test_authentication.py
@@ -24,3 +24,26 @@ def test_successful_login(requests, client):
         assert False, "Set-Cookie header is not found in login response"
 
 
+@patch("src.views.requests")
+def test_bad_login(requests, client):
+    email = "test@gmail.com"
+    password = "1234"
+
+    a = Mock(name="Responce", status_code=401)
+    requests.post.return_value = a
+
+    credentials = b64encode(f"{email}:{password}".encode())
+    resp = client.post(
+        "/login", headers={"Authorization": f"Basic {credentials.decode()}"}
+    )
+
+    assert resp.status_code == 401
+
+
+@patch("src.authentication.validate_jwt", return_value=(True, "some data"))
+def test_validate_token(validate_jwt, client):
+    client.set_cookie("JWT", "exp")
+    resp = client.get("/validate-token")
+
+    assert resp.status_code == 200
+    validate_jwt.assert_called_once()
