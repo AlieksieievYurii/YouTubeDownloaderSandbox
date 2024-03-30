ba43148a26eaab907596f2d18c63f6bd0ac68cfe
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2024-03-30 17:18:38 +0100

    Add optional env var represention token validation endpoint

--- front/src/config.ts
+++ front/src/config.ts
@@ -1,7 +1,7 @@
-
-export const LOGIN_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/login`
-export const QUEUE_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/queue`
-export const ITEMS_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/items`
-export const TERMINATE_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/terminate`
-export const RETRY_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/retry`
-export const DOWNLOAD_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/download`
+export const LOGIN_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/login`;
+export const VALIDATE_TOKEN_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_SIDE_GATEWAY_URL ? process.env.NEXT_PUBLIC_SERVER_SIDE_GATEWAY_URL : process.env.NEXT_PUBLIC_GATEWAY_URL}/validate-token`;
+export const QUEUE_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/queue`;
+export const ITEMS_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/items`;
+export const TERMINATE_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/terminate`;
+export const RETRY_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/retry`;
+export const DOWNLOAD_ENDPOINT = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/download`;
--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,14 +1,14 @@
-import axios from "axios";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+import { VALIDATE_TOKEN_ENDPOINT } from "./config";
 
 export async function middleware(request: NextRequest) {
   const redirectionRef = NextResponse.redirect(new URL("/login", request.url));
   const jwt = request.cookies.get("JWT");
   if (!jwt) return redirectionRef;
 
-  const resp = await fetch("http://localhost:8080/validate-token", {
+  const resp = await fetch(VALIDATE_TOKEN_ENDPOINT, {
     headers: {
       Cookie: `JWT=${jwt.value}`,
     },
